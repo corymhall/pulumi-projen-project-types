ba6190fa9430b69b0ce7b942710e1f8bffb4117d
corymhall <43035978+corymhall@users.noreply.github.com>
2025-03-14 07:58:13 -0400

    have to update the jobs differently

--- src/typescript.ts
+++ src/typescript.ts
@@ -20,6 +20,35 @@ export class TypeScriptComponent extends TypeScriptProject {
       },
     });
 
+    if (projenCredentials?.permissions) {
+      const upgradeWorkflows = this.upgradeWorkflow?.workflows;
+      upgradeWorkflows?.forEach((workflow) => {
+        const pr = workflow.getJob('pr');
+        workflow.updateJob('pr', {
+          ...pr,
+          permissions: {
+            ...pr.permissions,
+            ...projenCredentials?.permissions,
+          },
+        });
+      });
+      if (this.buildWorkflow && this.github) {
+        const buildName = this.buildWorkflow.name;
+        const buildWorkflow = this.github.tryFindWorkflow(buildName);
+        if (!buildWorkflow) {
+          throw new Error(`could not find build workflow ${buildName}`);
+        }
+        const mutationJob = buildWorkflow.getJob('self-mutation');
+        buildWorkflow?.updateJob('self-mutation', {
+          ...mutationJob,
+          permissions: {
+            ...mutationJob.permissions,
+            ...projenCredentials.permissions,
+          },
+        });
+      }
+    }
+
     const versionFile = this.package.file.path;
     const permissions = projenCredentials?.permissions;
 
